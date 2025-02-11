a7d606b46f13f7561cb38ddd746b9a63847a90ff
Lennard Reiker <lennard@MacBook-Pro-von-Lennard-2.local>
2025-02-11 09:45:00 +0100

    Formatierung

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,13 +6,13 @@ public class TicTacToe {
     public static void tictactoe(int tief, int hoch){
         for(int i=tief; i<=hoch; i++){
             String s = "";
-            if(i%2==0){
+            if(i % 2 == 0){
                 s = "tic";
             }
-            if(i%3==0){
+            if(i % 3 == 0){
                 s = s + "tac";
             }
-            if(i%2!=0 && i%3!=0){
+            if(i % 2 != 0 && i % 3 != 0){
                 s = s + "toe";
             }
             System.out.println(i + " " + s);
