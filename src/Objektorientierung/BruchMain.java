752e95250025a9e908764e65cda4d89a8c97ae53
Lennard Reiker <lennard@MacBook-Pro-von-Lennard-2.local>
2025-04-29 08:16:11 +0200

    Objektorientierung
    - Ausgabe in BruchMain noch angepasst, sodass auch direkt Doubles ausgegeben werden

--- src/Objektorientierung/BruchMain.java
+++ src/Objektorientierung/BruchMain.java
@@ -9,12 +9,10 @@ public class BruchMain {
         c = a.mult(b);
         d = a.add(b);
 
-        System.out.println(a.asDouble());
-
-        System.out.println("a = " +a);
-        System.out.println("b = " +b);
-        System.out.println("c = " +c);
-        System.out.println("d = " +d);
-        System.out.println("a = " +a);
+        System.out.println("a = " + a + " = " + a.asDouble());
+        System.out.println("b = " + b + " = " + b.asDouble());
+        System.out.println("c = " + c + " = " + c.asDouble());
+        System.out.println("d = " + d + " = " + d.asDouble());
+        System.out.println("a = " + a + " = " + a.asDouble());
     }
 }
