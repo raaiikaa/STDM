cab37d109c38a35a9dd2e55acabb197b50b1f5a8
Lennard Reiker <lennard@MacBook-Pro-von-Lennard-2.local>
2025-09-24 10:43:29 +0200

    Printer printArrayList hinzugefügt

--- src/GeneratorAndPrinter/Printer.java
+++ src/GeneratorAndPrinter/Printer.java
@@ -1,5 +1,7 @@
 package GeneratorAndPrinter;
 
+import java.util.ArrayList;
+
 public class Printer {
 
     public void printIntArray(int[] array){
@@ -7,6 +9,35 @@ public class Printer {
             System.out.print(array[i]);
             if(i < array.length - 1)
             System.out.print(",");
+        }
+    }
+
+    public void printIntArrayList(ArrayList<Integer> arrayList) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.print(arrayList.get(i));
+            if (i < arrayList.size() - 1)
+                System.out.print(",");
+        }
+    }
+
+    public void print2DimIntArray(int[][] array) {
+        String finalString = new String();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                finalString += Integer.valueOf(array[i][j]);
+            }
+            System.out.println(finalString);
+        }
+    }
+
+    public void print2DimIntArrayAsBingo(int[][] array) {
+        String finalString = "";
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                finalString += "  " + Integer.valueOf(array[i][j]);
+            }
+            System.out.println(finalString);
+            finalString = "";
         }
     }
 }
