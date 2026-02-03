c1da8f0d3053a7589e13b2f28a29e61addec6e77
Lennard Reiker <lennard@MacBook-Pro-von-Lennard-2.local>
2026-02-03 08:38:35 +0100

    - Kontoverwaltung Sparkonto

---
+++ src/Kontoverwaltung/Sparkonto.java
@@ -0,0 +1,10 @@
+package Kontoverwaltung;
+
+import java.util.Date;
+
+public class Sparkonto extends Konto {
+
+    public Sparkonto(String kontoInhaber, Date geburtsDatum, String adresse) {
+        super(kontoInhaber, geburtsDatum, adresse);
+    }
+}
